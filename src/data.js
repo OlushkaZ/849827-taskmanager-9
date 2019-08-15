57f405ddd39b17e17ebce1bac65547eeb7a1b881
OlushkaZ <gincgo@yandex.ru>
2019-08-16 00:07:55 +0300

    mock data

---
+++ src/data.js
@@ -0,0 +1,33 @@
+export const getTask = () => ({
+  description: [
+    `Изучить теорию`,
+    `Сделать домашку`,
+    `Пройти интенсив на соточку`,
+  ][Math.floor(Math.random() * 3)],
+  dueDate: Date.now() - 7 * 24 * 60 * 60 * 1000 + Math.floor(Math.random() * 14) * 24 * 60 * 60 * 1000,
+  tags: new Set([
+    `homework`,
+    `theory`,
+    `practice`,
+    `intensive`,
+    `keks`,
+  ]),
+  repeatingDays: {
+    'mo': false,
+    'tu': false,
+    'we': Boolean(Math.round(Math.random())),
+    'th': false,
+    'fr': Boolean(Math.round(Math.random())),
+    'sa': false,
+    'su': false,
+  },
+  color: [
+    `black`,
+    `yellow`,
+    `blue`,
+    `green`,
+    `pink`,
+  ][Math.floor(Math.random() * 5)],
+  isFavorite: Boolean(Math.round(Math.random())),
+  isArchive: Boolean(Math.round(Math.random())),
+});
